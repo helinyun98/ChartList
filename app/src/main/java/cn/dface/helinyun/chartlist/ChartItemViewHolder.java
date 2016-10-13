e0d0fc85d6d34ea214f0d485093fa233cebd4bb6
helinyun98 <helinyun98@me.com>
2016-10-13 16:52:36 +0800

    在ViewHolder中更新数据

--- app/src/main/java/cn/dface/helinyun/chartlist/ChartItemViewHolder.java
+++ app/src/main/java/cn/dface/helinyun/chartlist/ChartItemViewHolder.java
@@ -1,12 +1,17 @@
 package cn.dface.helinyun.chartlist;
 
+import android.graphics.PointF;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ChartItemViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView (R.id.chartItem)
+    ChartItemView chartItem;
 
     public static ChartItemViewHolder create(ViewGroup parent) {
         return new ChartItemViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.chart_item, parent, false));
@@ -18,6 +23,8 @@ public class ChartItemViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void update(ChartItemData pre, ChartItemData next) {
-
+        PointF prePoint = pre.getPoint();
+        PointF nextPoint = next.getPoint();
+        chartItem.updateData(prePoint, nextPoint);
     }
 }
